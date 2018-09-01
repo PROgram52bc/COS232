da281ba149ab7466aad6f06f31fcb68c7461d4da
DavidDeng <daviddenghaotian@163.com>
2018-08-31 23:09:49 -0400

    finished lab1

---
+++ course/cos232/labs/lab1/cmake-build-debug/CMakeFiles/lab1b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1b.dir/main.c.o"
+  "lab1b.pdb"
+  "lab1b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
