10de8a03998efe0c43eb371e3806195f07cdcdfb
DavidDeng <daviddenghaotian@163.com>
2018-09-08 22:39:21 -0400

    MILESTONE 2

--- course/cos232/project/steganography/encoder.c
+++ course/cos232/project/steganography/encoder.c
@@ -10,7 +10,44 @@ int main(int argc, char* argv[]) {
 	FILE* inputFile = fopen(argv[1], "r");
 	FILE* outputFile = fopen(argv[2], "w");
 	char* message = argv[3];
-	fprintf(stdout, "Your message is: %s\n", message);
+	// fprintf(stdout, "Your message is: %s\n", message);
+	char buf[BUFSIZ];
+	if (fgets(buf, 15, inputFile) == NULL) // 14 + 1 (\0)
+	{
+		fprintf(stderr, "Failed to read file %s.\n", argv[1]);
+		exit(-2);
+	}
+	if (buf[0] != 'B' || buf[1] != 'M')
+	{
+		fprintf(stderr, "Input file is not a bmp file.\n");
+		exit(-3);
+	}
+	// START processing header
+	int pixelPos = (int)buf[10];
+	fprintf(stdout, "Pixel Array Start at: %d.\n", pixelPos);
+	// END processing header
+
+	if (fgets(buf, pixelPos-14+1, inputFile) == NULL)
+	{
+		fprintf(stderr, "Failed to read file %s.\n", argv[1]);
+		exit(-2);
+	}
+	// START processing DIB header
+	char* pos = buf+4;
+	int width = *(int*)(pos);
+	pos+=4; // skip the width field
+	int height = *(int*)(pos);
+	pos+=6; // skip the height field and plane field
+	unsigned short bitsPerPixel = (pos[1] << 8) | pos[0]; // only for big endian system
+
+	fprintf(stdout, "Width: %d.\n", width);
+	fprintf(stdout, "Height: %d.\n", height);
+	// fprintf(stdout, "bit per pixel: %hu.\n", bitsPerPixel);
+
+	// END processing DIB header
+	
+
+
 	return 0;
 
 }
