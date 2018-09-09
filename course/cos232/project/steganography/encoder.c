053370718a87f8827aa42bf3a3edf89a4cd73ec9
DavidDeng <daviddenghaotian@163.com>
2018-09-09 17:44:02 -0400

    Writing to output file without encoding message, implementing using fread/fwrite instead of fgets/fputs

--- course/cos232/project/steganography/encoder.c
+++ course/cos232/project/steganography/encoder.c
@@ -12,9 +12,11 @@ int main(int argc, char* argv[]) {
 	char* message = argv[3];
 	// fprintf(stdout, "Your message is: %s\n", message);
 	char buf[BUFSIZ];
-	if (fgets(buf, 15, inputFile) == NULL) // 14 + 1 (\0)
+	int count;
+	count = fread(buf, 1, 14, inputFile);
+	if (count != 14) 
 	{
-		fprintf(stderr, "Failed to read file %s.\n", argv[1]);
+		fprintf(stderr, "Unexpected EOF.\n");
 		exit(-2);
 	}
 	if (buf[0] != 'B' || buf[1] != 'M')
@@ -22,16 +24,20 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "Input file is not a bmp file.\n");
 		exit(-3);
 	}
+
 	// START processing header
 	int pixelPos = (int)buf[10];
 	fprintf(stdout, "Pixel Array Start at: %d.\n", pixelPos);
+	fwrite(buf, 1, 14, outputFile); // parallel writing to output file.
 	// END processing header
 
-	if (fgets(buf, pixelPos-14+1, inputFile) == NULL)
+	count = fread(buf, 1, pixelPos-14, inputFile);
+	if (count != pixelPos-14) 
 	{
-		fprintf(stderr, "Failed to read file %s.\n", argv[1]);
+		fprintf(stderr, "Unexpected EOF.\n");
 		exit(-2);
 	}
+
 	// START processing DIB header
 	char* pos = buf+4;
 	int width = *(int*)(pos);
@@ -39,14 +45,16 @@ int main(int argc, char* argv[]) {
 	int height = *(int*)(pos);
 	pos+=6; // skip the height field and plane field
 	unsigned short bitsPerPixel = (pos[1] << 8) | pos[0]; // only for big endian system
-
 	fprintf(stdout, "Width: %d.\n", width);
 	fprintf(stdout, "Height: %d.\n", height);
-	// fprintf(stdout, "bit per pixel: %hu.\n", bitsPerPixel);
-
+	// fprintf(stdout, "bits per pixel: %hu.\n", bitsPerPixel);
+	fwrite(buf, 1, pixelPos-14, outputFile); // parallel writing to output file.
 	// END processing DIB header
-	
 
+	while (fread(buf, 1, 1, inputFile) == 1) // read byte by byte 
+	{
+		fwrite(buf, 1, 1, outputFile); // parallel writing to output file.
+	}
 
 	return 0;
 
