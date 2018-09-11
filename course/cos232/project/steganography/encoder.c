630aff7c3576a6f5ca141ccfcf58d6efeddc57c2
DavidDeng <daviddenghaotian@163.com>
2018-09-11 18:14:41 -0400

    encode complete (not bug-free)

--- course/cos232/project/steganography/encoder.c
+++ course/cos232/project/steganography/encoder.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
@@ -45,16 +46,54 @@ int main(int argc, char* argv[]) {
 	int height = *(int*)(pos);
 	pos+=6; // skip the height field and plane field
 	unsigned short bitsPerPixel = (pos[1] << 8) | pos[0]; // only for big endian system
+	if (bitsPerPixel != 24 && bitsPerPixel != 32) 
+	{
+		fprintf(stderr, "Unrecognized bits per pixel value: %hd", bitsPerPixel);
+		exit(-1);
+	}
 	fprintf(stdout, "Width: %d.\n", width);
 	fprintf(stdout, "Height: %d.\n", height);
 	// fprintf(stdout, "bits per pixel: %hu.\n", bitsPerPixel);
 	fwrite(buf, 1, pixelPos-14, outputFile); // parallel writing to output file.
 	// END processing DIB header
-
-	while (fread(buf, 1, 1, inputFile) == 1) // read byte by byte 
+	// START writing to output file with encoding
+	short bitCount = 0; // the current bit number to be read out of the 8 bits in a char. n E [0,7]
+	int letterIndex = 0; // the index of the current letter. n E [0,strlen(message)-1]
+	for (int i=0;;i++)
 	{
-		fwrite(buf, 1, 1, outputFile); // parallel writing to output file.
+		// checking conditions for exiting loops
+		if (bitsPerPixel == 32 && (i+1)%4==0) continue; // skip the Alpha byte
+		if (letterIndex > strlen(message)) // when finished reading the string (> because should include the final \0)
+		{
+			break; 
+		}
+		unsigned char mask = 1<<(7-bitCount);
+		unsigned char bit = (message[letterIndex] & mask) >> (7-bitCount); // the bitCount'th bit of the letter. n E {0,1}
+		unsigned char tmp;
+		if (fread(&tmp, 1, 1, inputFile) != 1) {
+			fprintf(stderr, "Image file terminated before all info are stored.\n");
+			exit(-1);
+		}
+		if (bit) // if should set 1
+			tmp |= 1;
+		else // if should set 0
+			tmp >> 1 << 1;
+		fprintf(stdout, "Writing %hd to file\n", bit);
+		fwrite(&tmp, 1, 1, outputFile); // parallel writing to output file.
+		// increment counts
+		if (bitCount<7) { 
+			bitCount++;
+		}
+		else {
+			bitCount = 0;
+			letterIndex++;
+			fprintf(stdout, "\n");
+		}
+
 	}
+	while (fread(buf, 1, 1, inputFile) == 1)
+		fwrite(buf, 1, 1, outputFile);
+	// END writing to output file with encoding
 
 	return 0;
 
