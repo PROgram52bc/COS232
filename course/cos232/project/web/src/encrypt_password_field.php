3259c6e3f8968ad9b734b92b48b47ad2d3c35682
DavidDeng <daviddenghaotian@163.com>
2018-11-19 03:44:16 +0000

    added utilties to do ugly rsa encryption with password field

---
+++ course/cos232/project/web/src/encrypt_password_field.php
@@ -0,0 +1,28 @@
+<?php /* To use this file: 
+There should be a file in the current directory called public.key,
+There should be a file in the current directory called jsencrypt.js, including the javascript implementation for rsa encryption,
+There should be a form element called 'userform',
+Inside 'userform, there should be a submit button, and a password field.
+As an output:
+A hidden field containing base-64 encoding of the password is appended to the form, with an id 'encrypted-password', and password field is emptied.
+The server side should do the reverse with a public key, which is extremly laborious and hard to debug and time consuming and not wise. But I really can't think of any other solution right now other than using https. SAD
+ */
+?>	
+<p id="public_key" hidden> <?php echo file_get_contents("public.key") ?></p>
+<script src="jsencrypt.js"></script>
+<script>
+console.log(userform);
+let submitButton = userform.querySelector("input[name=submit]");
+console.log(public_key.innerHTML);
+
+submitButton.addEventListener("click", function() {
+	// event.preventDefault();
+	let passwordField = userform.querySelector("input[name=password]");
+	let enc = new JSEncrypt();
+	enc.setPublicKey(public_key.innerHTML);
+	let encryptedPassword = enc.encrypt(passwordField.value);
+	console.log(encryptedPassword);
+	userform.insertAdjacentHTML('beforeend', `<input name='encrypted-password' value=${encryptedPassword} hidden></input>`);
+	passwordField.value = "";
+})
+</script>
---
+++ course/cos232/project/web/src/get_password.php
@@ -0,0 +1,14 @@
+<?php
+/* To use this function:
+		There should be a file called private.key including the private key
+		Pass the 64base encoded password as argument,
+		The plaintext password is returned. SAD SAD SAD :(
+*/
+function getPlainPassWord($encryptedPassword) {
+	$fp = fopen("private.key", "r");
+	$key = fread($fp, 8192);
+	openssl_private_decrypt(base64_decode($encryptedPassword), $plain_pass, $key);
+	var_dump($plain_pass);
+	return $plain_pass;
+}
+?>
